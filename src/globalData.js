90c93290b2842c9bcceeac2f714fab1e9856439b
Hugo kai <tank5460@hotmail.fr>
2023-03-16 14:02:30 +0100

    fix api https

--- src/globalData.js
+++ src/globalData.js
@@ -1,3 +1,3 @@
 global.api = {
-    url:'Cave88api-env-1.eba-biy3mymw.eu-west-3.elasticbeanstalk.com'
+    url:'https://test.d1rx9zqjy1rrr.amplifyapp.com/Cave88api-env-1.eba-biy3mymw.eu-west-3.elasticbeanstalk.com'
 };
